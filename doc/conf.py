bb9867bad437f493ddc702c11ac785a35e17b6ef
Kevin Thibedeau <kevin.thibedeau@gmail.com>
2017-04-20 23:15:40 -0400

    Add Symbolator and Graphviz extensions

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@ import alabaster
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -26,7 +26,7 @@ import alabaster
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.mathjax', 'alabaster', 'sphinxcontrib.vhdldomain']
+extensions = ['symbolator_sphinx', 'sphinx.ext.mathjax', 'alabaster', 'sphinxcontrib.vhdldomain', 'sphinx.ext.graphviz']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -87,6 +87,10 @@ pygments_style = 'sphinx'
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
 
+
+graphviz_output_format = 'svg'
+
+symbolator_cmd_args = ['-t']
 
 # -- Options for HTML output ---------------------------------------------------
 
